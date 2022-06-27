7bad044b5f4afc955b68766fcb30383956e79127
XimeFran <ximena.contreras.torres@ciisa.cl>
2022-06-27 19:56:27 -0400

    arreglosValidacion

--- Recurso/scripts/validaciones.js
+++ Recurso/scripts/validaciones.js
@@ -13,13 +13,13 @@ function obtenerDatos() {
             for (var indice = 0; indice < data.length; indice++) {
                 var fecha = data[indice].fecha
                 var nombre = data[indice].nombre
-                var ciudad = data[indice].ciudad
-                var avatar = data[indice].avatar
+                var Santiago = data[indice].Santiago
+                var foto = data[indice].foto
                 var filaTAbla = '<tr>'
                                 + '<td class="prc-25">'+fecha+'</td>'
                                 +  '<td class="prc-25">'+nombre+'</td>'
-                                +   '<td class="prc-25">'+ciudad+'</td>'
-                                +   '<td class="prc-25"><img src="'+avatar+'" alt="" style="width: 131px"></td>'
+                                +   '<td class="prc-25">'+Santiago+'</td>'
+                                +   '<td class="prc-25"><img src="'+foto+'" alt="" style="width: 131px"></td>'
                                 '</tr>'
                 console.log(filaTAbla)
                 $("#tabla-adopt").append(filaTAbla)
